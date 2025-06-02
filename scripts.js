f13534f3ea6c2a7eed7276fdfd4c1694d4b55e2a
SHIVAM KARN <karn48438@gmail.com>
2025-06-02 21:21:34 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Profile page with cool effects is loaded!");
+
+    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            target.scrollIntoView({ behavior: "smooth", block: "start" });
+        });
+    });
+});
